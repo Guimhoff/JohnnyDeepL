b0382ac3f19bdc72f7c222deb58762229bd3b9b4
Guillaume Imhoff <91632047+Guimhoff@users.noreply.github.com>
2023-02-05 20:57:42 +0100

    Base d'historique mais bug d'affichage

--- app/src/main/java/com/example/johnnydeepl/HistoryActivity.kt
+++ app/src/main/java/com/example/johnnydeepl/HistoryActivity.kt
@@ -2,10 +2,25 @@ package com.example.johnnydeepl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 
 class HistoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+        val historyView = findViewById<RecyclerView>(R.id.history_recycler_view)
+
+        val history = listOf<HistoryElement>(
+            HistoryElement("Texte source", "Texte Traduit", "EN", "EN", "FR"),
+            HistoryElement("Texte source 2", "Texte Traduit 2", "EN", "EN", "FR")
+        )
+
+
+        val historyAdapter = HistoryAdapter( this, history)
+        historyView.adapter = historyAdapter
+        historyView.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/com/example/johnnydeepl/HistoryAdapter.kt
+++ app/src/main/java/com/example/johnnydeepl/HistoryAdapter.kt
@@ -1,28 +1,33 @@
 package com.example.johnnydeepl
 
 import android.content.Context
-import android.widget.ArrayAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
-class HistoryAdapter: ArrayAdapter<HistoryElement> {
+class HistoryAdapter(private val context: Context, private val history: List<HistoryElement>) :
+    RecyclerView.Adapter<HistoryAdapter.ViewHolder>() {
 
-    val context: Context
-    val resource: Int
-    val history: Array<HistoryElement>
-
-
-    constructor(
-        context: Context,
-        resource: Int,
-        history: Array<out HistoryElement>
-    ) : super(context, resource, history) {
-        this.context = context
-        this.resource = resource
-        this.history = history as Array<HistoryElement>
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textSource: TextView = itemView.findViewById<TextView>(R.id.originalTextView)
+        val textTranslated: TextView = itemView.findViewById<TextView>(R.id.translatedTextView)
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.history_item, parent, false)
+        return ViewHolder(view)
+    }
 
+    override fun getItemCount(): Int {
+        return history.size
+    }
 
-
-
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textSource.text = history[position].textSource
+        holder.textTranslated.text = history[position].textTranslated
+    }
 }
+
 
